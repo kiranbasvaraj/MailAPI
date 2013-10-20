53372702bfe534c983fc388c536d72c9d0b25e9b
pooran <pooran@live.com>
2013-10-20 04:00:30 -0700

    First Commit

---
+++ MailAPI/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+
+namespace MailAPI.Controllers
+{
+    public class MailController : ApiController
+    {
+        // GET api/sendmail
+        public string Get(string from, string frompassword, string to, string cc, string subject, string body)
+        { 
+            MailAddress fromAddress = new MailAddress(from);
+            MailAddress toAddress = new MailAddress(to);
+            MailAddress ccAddress = null;
+            if (cc != "")
+                ccAddress = new MailAddress(cc);
+           
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, frompassword)
+            };
+            var message = new MailMessage(fromAddress, toAddress);
+            message.Subject = subject;
+            message.Body = body;
+            message.CC.Add(ccAddress);
+            smtp.Send(message);
+            return "success";
+        }
+    }
+}
